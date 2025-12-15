a9d6953819fea3fc74bc4e395b85360e12b0cf84
TiwariSudhanshu <sudhanshutiwari9836@gmail.com>
2025-12-15 17:31:33 +0530

    Changed url

--- admin/src/lib/api.ts
+++ admin/src/lib/api.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 // Single source of truth for backend URL
-// const API_BASE_URL = 'https://api.ezmasalaa.com/api';
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://api.ezmasalaa.com/api';
+// const API_BASE_URL = 'http://localhost:5000/api';
 
 const axiosInstance = axios.create({
   baseURL: API_BASE_URL,
